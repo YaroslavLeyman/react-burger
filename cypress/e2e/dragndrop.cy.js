2519288600bf738026856dcad1f6d3d857c63b00
Iaroslav Leiman <yaroslavleiman@yandex.ru>
2023-02-18 15:58:48 +0700

    Корректная работа теста dnd

--- cypress/e2e/dragndrop.cy.js
+++ cypress/e2e/dragndrop.cy.js
@@ -9,14 +9,21 @@ describe("Drag'n'Drop", () => {
   describe("Перетаскивание ингредиентов в конструктор", () => {
 
     it("Перетаскивание булки", () => {
-      cy.get(selectors.ingredients.ingredient + ":eq(0)").trigger('dragstart');
-      cy.get(selectors.constructor.container).trigger('drop');
-      cy.get(selectors.constructor.bunTop).should("exist");
-      cy.get(selectors.constructor.bunBottom).should("exist");
 
-      cy.get(selectors.ingredients.ingredient + ":eq(3)").trigger('dragstart');
-      cy.get(selectors.constructor.container).trigger('drop');
-      cy.get(selectors.constructor.innerItems).should("not.be.empty");
+       // Ждем загрузку первого ингредиента
+       cy.wait(1000);
+       cy.get(selectors.ingredients.ingredient + ":eq(0)").trigger('dragstart');
+
+       cy.get(selectors.constructor.container).should('exist');
+       cy.get(selectors.constructor.container).trigger('drop');
+
+       cy.get(selectors.constructor.bunTop).should("exist");
+       cy.get(selectors.constructor.bunBottom).should("exist");
+
+       cy.get(selectors.ingredients.ingredient + ":eq(3)").trigger('dragstart');
+       cy.get(selectors.constructor.container).trigger('drop');
+       cy.get(selectors.constructor.innerItems).should("not.be.empty");
+
     });
 
   })
